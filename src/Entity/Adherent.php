9f66705c9f1fe06d6f980a7a173cb10e77094c31
Alexis Bassignot <misterbalise2@gmail.com>
2023-10-18 08:46:30 +0200

    update Adherent.php

--- src/Entity/Adherent.php
+++ src/Entity/Adherent.php
@@ -3,18 +3,98 @@
 namespace App\Entity;
 
 use DateTime;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
 
+#[Entity]
+#[Table(name: 'Adherent')]
 class Adherent
 {
+    #[Id]
+    #[Column(type: 'integer')]
+    #[GeneratedValue]
     private int $id;
-    private string $numeroadherent;
+
+    #[Column(length: 9)]
+    private string $numeroAdherent;
+
+    #[Column(length: 50)]
     private string $nom;
+
+    #[Column(length: 50)]
     private string $prenom;
+
+    #[Column(length: 140)]
     private string $email;
+
+    #[Column(type: 'date')]
     private ?DateTime $dateAdhesion;
 
     public function __construct()
     {
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getNumeroAdherent(): string
+    {
+        return $this->numeroAdherent;
+    }
+
+    public function setNumeroAdherent(string $numeroAdherent): void
+    {
+        $this->numeroAdherent = $numeroAdherent;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): void
+    {
+        $this->prenom = $prenom;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getDateAdhesion(): ?DateTime
+    {
+        return $this->dateAdhesion;
+    }
+
+    public function setDateAdhesion(?DateTime $dateAdhesion): void
+    {
+        $this->dateAdhesion = $dateAdhesion;
     }
 
 }
